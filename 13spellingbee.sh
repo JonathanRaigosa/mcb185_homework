cd1d411bc252e21f757d28c62dbe1d31b63832e6
JonathanRaigosa <jcraigosa@gmail.com>
2024-01-20 22:26:49 -0800

    simplify

--- 13spellingbee.sh
+++ 13spellingbee.sh
@@ -1,1 +1,3 @@
- gunzip -c ~/Code/MCB185/data/dictionary.gz | grep -E ".{4,}" | grep -E ".*r.*" | grep -v "b" | grep -v "d" | grep -v "e" | grep -v "f" | grep -v "g" | grep -v "h" | grep -v "j" | grep -v "k" | grep -v "l" | grep -v "m" | grep -v "p" | grep -v "q" | grep -v "s" | grep -v "t" | grep -v "u" | grep -v "v" | grep -v "w" | grep -v "x" | grep -v "y"
+ #gunzip -c ~/Code/MCB185/data/dictionary.gz | grep -E ".{4,}" | grep -E ".*r.*" | grep -v "b" | grep -v "d" | grep -v "e" | grep -v "f" | grep -v "g" | grep -v "h" | grep -v "j" | grep -v "k" | grep -v "l" | grep -v "m" | grep -v "p" | grep -v "q" | grep -v "s" | grep -v "t" | grep -v "u" | grep -v "v" | grep -v "w" | grep -v "x" | grep -v "y"
+ # This  was a round about way of doing the second one
+ gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "r" | grep -E "[zonica]+" | grep -v -E "[^zonicar]+" | grep -E ".{4,}"
