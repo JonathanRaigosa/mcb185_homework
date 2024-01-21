5bb56e76658ae8e2e77c9af750a635741862766f
JonathanRaigosa <jcraigosa@gmail.com>
2024-01-20 22:09:21 -0800

    new

---
+++ 13spellingbee.sh
@@ -0,0 +1,1 @@
+ gunzip -c ~/Code/MCB185/data/dictionary.gz | grep -E ".{4,}" | grep -E ".*r.*" | grep -v "b" | grep -v "d" | grep -v "e" | grep -v "f" | grep -v "g" | grep -v "h" | grep -v "j" | grep -v "k" | grep -v "l" | grep -v "m" | grep -v "p" | grep -v "q" | grep -v "s" | grep -v "t" | grep -v "u" | grep -v "v" | grep -v "w" | grep -v "x" | grep -v "y"
