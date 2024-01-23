0a58b8e06d41a82932fab7c62ed4243d6fb8d083
JonathanRaigosa <jcraigosa@ucdavis.edu>
2024-01-23 10:01:59 -0800

    new

---
+++ quiz1.sh
@@ -0,0 +1,7 @@
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep a | grep -v -E "[^uomtfca]+"
+
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep b | grep -v -E "[^ailnrtb]+"
+
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep m | grep -v -E "[^caonidm]+"
+
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep z | grep -v -E "[znorgia]+"
