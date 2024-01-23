209a04422c192c98b72adfa80e7dac8aca3a0c4c
JonathanRaigosa <jcraigosa@ucdavis.edu>
2024-01-23 10:46:58 -0800

    new

--- quiz1.sh
+++ quiz1.sh
@@ -5,3 +5,17 @@ gunzip -c ~/Code/MCB185/data/dictionary.gz | grep b | grep -v -E "[^ailnrtb]+"
 gunzip -c ~/Code/MCB185/data/dictionary.gz | grep m | grep -v -E "[^caonidm]+"
 
 gunzip -c ~/Code/MCB185/data/dictionary.gz | grep z | grep -v -E "[znorgia]+"
+
+gunzip -c ~/Code/MCB185/data/jaspar2024_core.transfac.gz | grep diatoms
+
+gunzip -c ~/Code/MCB185/data/jaspar2024_core.transfac.gz | grep urochordates
+
+gunzip -c ~/Code/MCB185/data/jaspar2024_core.transfac.gz | grep nematodes
+
+gunzip -c ~/Code/MCB185/data/jaspar2024_core.transfac.gz | grep fungi
+
+gunzip -c ~/Code/MCB185/data/jaspar2024_core.transfac.gz | grep insects
+
+gunzip -c ~/Code/MCB185/data/jaspar2024_core.transfac.gz | grep plants
+
+gunzip -c ~/Code/MCB185/data/jaspar2024_core.transfac.gz | grep vertebratessh 
