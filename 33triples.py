d9f60488f4de517a8310f26663b226f4459610b2
JonathanRaigosa <jcraigosa@gmail.com>
2024-02-03 20:57:12 -0800

    new

--- 33triples.py
+++ 33triples.py
@@ -6,11 +6,14 @@ def perfect_square(n):
 	if math.isclose(math.sqrt(n), math.sqrt(n) // 1): 
 		return True
 	return False
+# Checks to see if perfect square
 
 def triples(n):
 	for i in range(1, n):
-		for y in range(1, n):
+		for y in range(i + 1, n):
+# Nested Loops
 			if perfect_square(i**2 + y**2) and i < y:
 				print(i, y, int(math.sqrt(i**2 + y**2)))
+# Calculates the square root of c...
 				
 triples(100)
