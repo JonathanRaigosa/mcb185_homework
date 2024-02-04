fe73a3961566e155d60b0eb8f5037687ee95ccc6
JonathanRaigosa <jcraigosa@gmail.com>
2024-02-03 20:15:29 -0800

    new

--- 34scoringmatrix.py
+++ 34scoringmatrix.py
@@ -2,11 +2,13 @@
 
 sequence = 'ACGT'
 print('   ', end=' ')
-# Print provides a space and terminates
+# Print provides a space and terminates, the space aligns the rows.
 
 for i in sequence:
 	print(i, end='  ')
 print()
+# For the heading, prints each letter separate.
+
 
 for seq_1 in sequence:
 	print(seq_1, end='  ')
@@ -14,7 +16,8 @@ for seq_1 in sequence:
 		if seq_1 == seq_2: print('+1', end=' ')
 		else: print('-1', end=' ')
 	print()
-	
+
+#Prints numbers corresponding to A C G T
 
 # The spaces are required for the matrix spacing.
 
